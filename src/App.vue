46d32070e34e1b42e7ea7c2f8f37b1c2a4980076
Chinsyo <chinsyo@users.noreply.github.com>
2016-12-14 18:51:06 +0800

    Update App.vue
    fix typo lnag

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 }
 </script>
 
-<style lnag='scss'>
+<style lang='scss'>
 @import './assets/styles/icon.css';
 @import './assets/styles/reset.css';
 @import './assets/styles/video-js.css';
